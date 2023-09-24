bd5fc45122f8762892d4c5ce4c087374bf7796fe
timmoh-king <timothykariuki43@gmail.com>
2023-09-24 21:59:04 +0300

    optional chaining

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let sales = 12345678;
 let course = "TypeScript";
 let is_published = false;
@@ -6,4 +7,44 @@ let level;
 function render(document) {
     console.log(document);
 }
+let numbers = [1, 2, 3];
+let unit = [1042, 'Technical writing'];
+;
+let tshirt = "l";
+console.log(tshirt);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear > 2022)
+        return income * 0.2;
+    return income * 0.1;
+}
+console.log(calculateTax(20000, 2023));
+let employee = {
+    id: 1,
+    name: "ALX SE",
+    retire: (date) => console.log(date)
+};
+console.log(employee.name);
+function kiloToGram(weight) {
+    if (typeof (weight) === 'number')
+        return weight * 2;
+    else
+        return parseInt(weight) * 4;
+}
+console.log(kiloToGram(10));
+console.log(kiloToGram('10'));
+let textBox = {
+    drag: () => console.log('dragging'),
+    click: () => console.log('clicking')
+};
+let feedback = 'pending';
+console.log(feedback);
+function greet(name) {
+    return `Hello ${name}`;
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,97 @@
+//number
 let sales = 123_45678;
+
+//string
 let course = "TypeScript";
+
+//boolean
 let is_published = false;
+
+//any
 let level;
 
+//string
 function render (document: string) {
     console.log(document);
 }
+
+//array
+let numbers: number[] = [1, 2, 3];
+
+//tuple - for key value pairs
+let unit: [number, string] = [1042, 'Technical writing'];
+
+//enums use PascalCase
+const enum Size {Small = 's', Medium = 'm', Large = 'l'};
+let tshirt: Size = Size.Large;
+console.log(tshirt);
+
+//functions
+function calculateTax (income: number, taxYear = 2022) {
+    if (taxYear > 2022) 
+        return income * 0.2
+    return income * 0.1
+}
+console.log(calculateTax(20000, 2023))
+
+//objects
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+let employee: Employee = {
+    id: 1,
+    name: "ALX SE",
+    retire: (date: Date) => console.log(date)
+};
+console.log(employee.name);
+
+//Union
+function kiloToGram (weight: number | string): number {
+    if (typeof(weight) === 'number')
+        return weight * 2
+    else
+        return parseInt(weight) * 4
+}
+console.log(kiloToGram(10));
+console.log(kiloToGram('10'));
+
+//Intersection types
+type Draggable = {
+    drag: () => void
+}
+
+type Clickable = {
+    click: () => void
+}
+
+type UIwidget = Draggable & Clickable;
+
+let textBox: UIwidget = {
+    drag: () => console.log('dragging'),
+    click: () => console.log('clicking')
+}
+
+//Literal types
+type Status = 'success' | 'failure' | 'pending';
+let feedback: Status = 'pending';
+console.log(feedback);
+
+//nullable types
+function greet(name: string | null | undefined): string {
+    return `Hello ${name}`;
+}
+greet(null)
+
+//optional chaining
+type Customer = {
+    birthday: Date
+}
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : {birthday: new Date()}
+}
+
+let customer = getCustomer(1);
+console.log(customer?.birthday?.getFullYear())
